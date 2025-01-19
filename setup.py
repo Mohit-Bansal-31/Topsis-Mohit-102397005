035e3800034f2449933a32aefc001bf61d0cd915
Mohit Bansal <mohitbansal0031@gmail.com>
2025-01-20 02:22:28 +0530

    version changed of setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Topsis technique for MCDM'
 from pathlib import Path
 this_directory = Path(__file__).parent
