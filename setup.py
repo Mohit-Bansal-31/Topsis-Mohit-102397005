1ee4f7dad22299e6ec5e1eff9401012c6245b77d
Mohit Bansal <mohitbansal0031@gmail.com>
2025-01-21 22:31:18 +0530

    change package name in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '0.0.28'
+VERSION = '0.0.31'
 DESCRIPTION = 'Topsis technique for MCDM'
 from pathlib import Path
 this_directory = Path(__file__).parent
@@ -10,7 +10,7 @@ LONG_DESCRIPTION = (this_directory / "README.md").read_text()
 
 # Setting up
 setup(
-    name="Topsis-Mohit-102397005",
+    name="Topsis_Mohit_102397005",
     version=VERSION,
     author="Mohit Bansal",
     author_email="mohitbansal0031@gmail.com",
